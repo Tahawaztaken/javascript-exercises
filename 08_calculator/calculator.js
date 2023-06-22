a4e0f60c92434f00512d721f7b95af8a0f2364b3
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-06-22 11:59:50 -0400

    08-complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  let sumArray = 0
+  for (let i = 0; i < myArray.length; i++){
+    sumArray += myArray[i]
+  }
+  return sumArray
 };
 
-const multiply = function() {
-
+const multiply = function(num1, ...numN) {
+  let sum = num1;
+  for (let i = 0; i < numN.length; i++) {
+    sum *= numN[i]
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    return (x > 1) ? x * factorial(x-1) : 1;
 };
 
 // Do not edit below this line
