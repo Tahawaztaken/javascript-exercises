27489c55b5033f86600c432718736be0700da161
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-06-22 13:22:21 -0400

    09 - completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+//   if (palindrome.length % 2 != 0) {
+    
 
+    beginning = palindrome.slice(0, Math.floor(palindrome.length / 2))
+    end = palindrome.slice(Math.ceil(palindrome.length / 2))
+
+    if (beginning == end.split("").reverse().join("")){
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
