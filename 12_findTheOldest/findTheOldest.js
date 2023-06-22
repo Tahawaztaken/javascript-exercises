948cbacc6fb59fffe86698deb304c2dfe2b82813
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-06-22 14:44:04 -0400

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+"use strict";
+const findTheOldest = function(people) {
+    let presentYear = 2023
+    let oldestPerson = null;
+    let oldestAge = 0
 
+    for (let i = 0; i < people.length; i++) {
+        if (people[i]["yearOfDeath"] === undefined) {
+            let age = 2023 - people[i]["yearOfBirth"]
+            if (age > oldestAge) {
+                oldestPerson = people[i]
+                oldestAge = age
+            }
+        }
+        let age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"]
+        if (age > oldestAge) {
+            oldestPerson = people[i]
+            oldestAge = age
+        }
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
